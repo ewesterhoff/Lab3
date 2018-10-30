a3719dbfddb586b65842b38072ae47f2a39a0717
ewesterhoff <emma.westerhoff@students.olin.edu>
2018-10-30 11:19:06 -0400

    assembly test completed

--- asm_tests_E&M/asm_mirror.asm
+++ asm_tests_E&M/asm_mirror.asm
@@ -21,20 +21,20 @@ addi	$t7, $zero, 7		# 8 elements in array
 # Initialize variables
 addi	$t2, $zero, 0		# i, the current array element being accessed
 addi	$t3, $t0, 0		# address of my_array[i] (starts from base address for i=0)
+addi 	$t6, $t3, 28
 
 LOOPSTART:
 beq 	$t2, $t1, LOOPEND	# if i == 4: GOTO DONE
-sub	$t6, $t7, $t2		# calculate 8-i index and story in t6
-sll 	$t6, $t6, 2
 
-lw	$t4, 0($t3)		# temp1 = my_array[i]		{LOAD FROM MEMORY}
-lw	$t5, 0($t6)		# temp2 = my_array[8-i]		{LOAD FROM MEMORY}
+lw	$t4, ($t3)		# temp1 = my_array[i]		{LOAD FROM MEMORY}
+lw	$t5, ($t6)		# temp2 = my_array[8-i]		{LOAD FROM MEMORY}
 
-sw	$t4, 0($t6)		# my_array[i] = temp1		{STORE TO MEMORY}
-sw	$t5, 0($t3)		# my_array[8-i] = temp2		{STORE TO MEMORY}
+sw	$t4, ($t6)		# my_array[i] = temp1		{STORE TO MEMORY}
+sw	$t5, ($t3)		# my_array[8-i] = temp2		{STORE TO MEMORY}
 
 addi	$t2, $t2, 1		# increment i counter
 addi	$t3, $t3, 4		# increment address by 1 word
+subi	$t6, $t6, 4		# increment address back by 1 word
 j	LOOPSTART		# GOTO start of loop
 LOOPEND:
 j	LOOPEND			# Jump trap prevents falling off end of program
@@ -44,7 +44,7 @@ j	LOOPEND			# Jump trap prevents falling off end of program
 #  Note that I have given the data values a distinctive pattern to help with debugging
 .data 
 my_array:
-0x00000000	# my_array[0]
+0x11110000	# my_array[0]
 0x22220000
 0x44440000
 0x66660000
