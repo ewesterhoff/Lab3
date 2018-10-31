aed2c277b374066255daa1dd6f57a35df954105c
ewesterhoff <emma.westerhoff@students.olin.edu>
2018-10-30 22:30:44 -0400

    jump instruction

--- module_blocks.v
+++ module_blocks.v
@@ -1,7 +1,7 @@
 
 `include "alu.v"
 `include "lut.v"
-
+/*
 module JAL_module
 (
 output[31:0] toDataW,
@@ -12,7 +12,6 @@ input[5:0] OPCode
   parameter constIncr = 32'b00000000000000000000000000000100; //4
   wire[31:0] adder_out;
   wire carryout, zero, overflow, if_jal;
-  wire[31:0] mux_out;
 
 ALU alu(.result(adder_out), .carryout(carryout), .zero(zero), .overflow(overflow),
 .operandA(constIncr), .operandB(PC), .command(3'd0)); //add
@@ -37,5 +36,25 @@ begin
      mux_out = din_1 ;
  end
  end
+
+endmodule
+
+*/
+module Jump_Calc
+(
+output[31:0] new_PC,
+input[31:0]	alu_output,
+input[31:0] old_PC,
+input[31:0] instruction,
+input[5:0] OPCode
+);
+  wire if_jump;
+  reg[31:0] cat = {instruction[0:25], old_PC[28:31]};
+always @ (cat)
+  cat = cat<<2;
+
+JumpLUT JTEST(.muxindex(if_jump), .OPCode(OPCode));
+
+doublemux32 mux(.din_0(cat), .din_1(alu_output), .sel(if_jump), .mux_out(new_PC));
 
 endmodule
