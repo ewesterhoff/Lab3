6ea99d368e78bbb265669620bc221df147e35a85
marches <marchsaper@gmail.com>
2018-10-31 21:38:52 -0400

    fixed compile errors

--- cpu.v
+++ cpu.v
@@ -1,5 +1,6 @@
 `include "register.v"
 `include "functionality_support.v"
+`include "alu.v"
 
 module cpu
 (
@@ -7,10 +8,11 @@ module cpu
 
 	input RegWr,
 	input[1:0] RegDst,
-	input ALUcntrl,
+	input[2:0] ALUcntrl,
 	input MemWr,
 	input MemToReg,
-	
+	input ALUSrc,
+
 	input[15:0] imm16,
 
 	input[4:0] Rd,
@@ -20,8 +22,8 @@ module cpu
 
 	input[31:0] Jal_out,
 
-	output reg[31:0] Op_end_result,
-	output reg Alu_zero, Alu_carryout, Alu_overflow
+	output [31:0] Op_end_result,
+	output Alu_zero, Alu_carryout, Alu_overflow
 );
 
 	wire[31:0] Da, Db, Alu_bin, DataMem_out, Op_end_result, Alu_op_result, immSE;
@@ -38,7 +40,8 @@ module cpu
 
 	signextend SE(.short(imm16), .long(immSE));
 
-	doublemux32 SEMux(.din_0(Db), .din_1(immSE), .sel(RegDst), 
+	// 0 - Db, 1 - SE
+	doublemux32 SEMux(.din_0(Db), .din_1(immSE), .sel(ALUSrc), 
 		.mux_out(Alu_bin));
 
 	ALU cpuAlu(.result(Alu_op_result), .carryout(Alu_carryout),
@@ -49,7 +52,7 @@ module cpu
 		.writeEnable(MemWr), .dataIn(Db));
 
 	doublemux32 MemRegMux(.din_0(Alu_op_result), .din_1(DataMem_out), 
-		.sel MemToReg), .mux_out(Op_end_result));
+		.sel(MemToReg), .mux_out(Op_end_result));
 
 
 endmodule
