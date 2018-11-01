1bb49f4346faa09879602f290b69ca491028f404
ewesterhoff <emma.westerhoff@students.olin.edu>
2018-11-01 11:44:36 -0400

    where is mars

---
+++ settings.mk
@@ -0,0 +1,18 @@
+# Project-specific settings
+
+## Assembly settings
+
+# Assembly program (minus .asm extension)
+PROGRAM := addi
+
+# Memory image(s) to create from the assembly program
+MEMDUMP := $(PROGRAM).text.hex
+
+
+## Verilog settings
+
+# Top-level module/filename (minus .v/.t.v extension)
+TOPLEVEL := singleCycleCPU
+
+# All circuits included by the toplevel $(TOPLEVEL).t.v
+CIRCUITS := $(TOPLEVEL).v
